942ab29680c8f6d6a53eb7ae7578f37431fb10cf
Suki <wangahuyi0327@gmail.com>
2024-05-26 08:23:47 +0800

    created db

--- app.py
+++ app.py
@@ -1,23 +1,35 @@
 #file name must be app.py
 from flask import Flask, jsonify, render_template, url_for
 from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
 
-app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI']-'sqlite'
+my_app = Flask(__name__)
+my_app.config['SQLALCHEMY_DATABASE_URI']='sqlite:///test.db' # create & specify database location
+db = SQLAlchemy(my_app) # initalize database 
 
-@app.route("/")
+class Todo(db.Model): # create a db model
+    id = db.Column(db.Integer, primary_key=True) #set up an id column
+    content = db.Column(db.String(200), nullable=False)
+    date_created = db.Column(db.DateTime, default=datetime.utcnow)
+    
+    def __repr__(self):
+        return '<Task %r>' %self.id
+
+
+
+@my_app.route("/")
 def helloWorld():
     return "<b>my first Flask application in action!</b>"
 
-@app.route('/json')
+@my_app.route('/json')
 def json_example():
     return jsonify(message="Hello, JSON!")
     #return {"message": "Hello, JSON!"}  #if not using jsonify
 
-@app.route('/htmlTemplate')
+@my_app.route('/htmlTemplate')
 def indexabc():
     return render_template('index.html')  # folder name must be "templates" for render_template function to recgonize
 
 if __name__ == '__main__':
-    app.run(debug=True)
-    # CLI To run the app: flask run
+    my_app.run(debug=True)
+    # Cmd To run the app: flask run
